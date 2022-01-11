04763a5318be83676984040a036482b3cce2eba6
emeery <gerardoemery@live.com.mx>
2022-01-11 17:55:10 -0600

    meter

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -3,6 +3,7 @@ import { MatTableDataSource } from '@angular/material/table';
 import { APIServiceService } from 'src/app/api-service.service';
 import { Site } from 'src/app/models/site.model';
 
+
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
@@ -11,8 +12,7 @@ import { Site } from 'src/app/models/site.model';
 export class TableComponent implements OnInit {
   displayedColumns: string[] = ['id', 'title', 'completed', 'userId']
   dataSource = new MatTableDataSource()
-  data: Site[] = []
-  filterValue:string = '';
+  // data: Site[] = []
   constructor(private service: APIServiceService) {
   }
 
@@ -30,10 +30,6 @@ export class TableComponent implements OnInit {
   applyFilter(filterValue:string) {
     this.dataSource.filter = filterValue
   }
-
-  getMessage(message: string) {
-    this.filterValue = message;
-}
 
   filterPredicate(data:any, filter: string) {
     let datas = JSON.stringify(data).includes(filter)
