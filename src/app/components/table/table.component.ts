5a7ac29d2b632d477d75e5d60f0ffcd9bb203551
emeery <gerardoemery@live.com.mx>
2022-02-11 13:38:34 -0600

    filtered data

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,6 +1,7 @@
 import { SelectionModel } from '@angular/cdk/collections';
 import { Component, OnInit } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
+import { filter } from 'rxjs';
 import { APIServiceService } from 'src/app/api-service.service';
 import { Site } from 'src/app/models/site.model';
 
@@ -14,21 +15,44 @@ export class TableComponent implements OnInit {
   displayedColumns: string[] = ['id', 'title', 'completed', 'userId']
   dataSource = new MatTableDataSource<Site>()
   selection = new SelectionModel<Site>(true, []);
-  color: string = ''
-  filterObject = {name:'', domain: ''}
-  isChecked = false;
+  filters: string[] = [];
+  filteredData: any[] = [];
+  originalData: any[] = [];
+
   constructor(private service: APIServiceService) {
   }
 
   ngOnInit(): void {
-    this.dataSource.filterPredicate = this.filterPredicate
     this.getData()
+    this.dataSource.filterPredicate = this.filterPredicate
   }
 
   getData() {
     this.service.getData().subscribe((data:Site[]) => {
+      this.originalData = this.dataSource.data
       this.dataSource.data = data
+
     })
+  }
+
+  onFilter(filterValues: string): void {
+    if (!this.filters.includes(filterValues)) {
+      this.filters.push(filterValues);
+    } else {
+      const index = this.filters.indexOf(filterValues);
+      this.filters.splice(index, 1);
+    }
+    if (this.filters.length === 0) {
+      this.dataSource.data = this.originalData;
+    } else {
+      console.log(filterValues)
+      this.filteredData = this.dataSource.data.filter((site) => {
+        console.log('site', site )
+        return this.filters.includes(site.accessMedia);
+      });
+      console.log('filterdata',this.filteredData)
+      this.dataSource.data = this.filteredData;
+    }
   }
 
   isAllSelected() {
@@ -60,6 +84,8 @@ export class TableComponent implements OnInit {
   }
 
   filterPredicate(data:any, filter: string) {
+    console.log('data',data)
+    console.log('filter',filter);
     let datas = JSON.stringify(data).includes(filter)
     return datas
   }
